583f45cc24165472440a0f74e69d115481340861
keyue <1281926469@qq.com>
2022-06-07 08:47:10 +0800

    优化了一点参数判断异常处理

--- src/main/java/com/guji/promotion/impl/PromotionDiscount.java
+++ src/main/java/com/guji/promotion/impl/PromotionDiscount.java
@@ -5,6 +5,7 @@ import com.guji.entity.VO.GoodsVO;
 import com.guji.entity.VO.OrderVO;
 import com.guji.promotion.PromotionHandler;
 import com.guji.promotion.PromotionType;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -25,7 +26,14 @@ public class PromotionDiscount implements PromotionHandler {
         Integer totalPrice = 0;
         List<GoodsVO> goodsList = orderVO.getGoodsList();
         for (GoodsVO goodsVO : goodsList) {
-            Fruit fruit = (Fruit) applicationContext.getBean(goodsVO.getName());
+            Fruit fruit = null;
+            try {
+                //通过商品名称获取bean
+                fruit = (Fruit) applicationContext.getBean(goodsVO.getName());
+            } catch (BeansException e) {
+                e.printStackTrace();
+                throw new RuntimeException("获取bean失败, 可能是水果名称不正确");
+            }
             if (fruit.getName().equals("Strawberry")) {
                 float Price = fruit.getPrice() * goodsVO.getCount() * 0.8F;
                 totalPrice += (int) Price;
--- src/test/java/com/guji/ICBCTest.java
+++ src/test/java/com/guji/ICBCTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ICBCTest {
@@ -20,11 +21,20 @@ public class ICBCTest {
     private PromotionHandlerContext promotionHandlerContext;
 
     @Test
-    public void test() {
+    public void test_ICBC() {
         //请输入优惠模式：1:不打折，2:草莓打八折，3:草莓打八折并且满100减10 (输入其他默认为不打折)
-        int score = 2;
+        int score = 1;
 
-        //1.准备好策略
+        //1.准备一些测试数据(水果名需要为spring容器中的bean名)
+        GoodsVO apple = new GoodsVO("apple", 9);    //苹果及数量
+        GoodsVO mango = new GoodsVO("mango", 6);    //芒果及数量
+        GoodsVO strawberry = new GoodsVO("strawberry", 9);  //草莓及数量
+        OrderVO orderVO = new OrderVO();
+        orderVO.addGoods(apple);
+        orderVO.addGoods(mango);
+        orderVO.addGoods(strawberry);
+
+        //2.准备好促销策略
         String promotion = "";
         switch (score) {
             case 1:
@@ -41,20 +51,23 @@ public class ICBCTest {
                 break;
         }
 
-        //2.准备一些测试数据
-        GoodsVO apple = new GoodsVO("apple", 1);    //苹果及数量
-        GoodsVO mango = new GoodsVO("mango", 2);    //芒果及数量
-        GoodsVO strawberry = new GoodsVO("strawberry", 8);  //草莓及数量
-        OrderVO orderVO = new OrderVO();
-        orderVO.addGoods(apple);
-        orderVO.addGoods(mango);
-        orderVO.addGoods(strawberry);
+        //3.预先判断下购买数量有没有问题
+        orderVO.getGoodsList().stream().filter(goodsVO -> goodsVO.getCount() < 1).forEach(goodsVO -> {
+            throw new RuntimeException("商品购买数量不能小于1," + goodsVO.getName() + "当前购买数量为" + goodsVO.getCount());
+        });
 
-        //3.开始买
+        //3.开始买(通过策略上下文获取策略实现类)
         PromotionHandler promotionHandler = promotionHandlerContext.getPromotionHandler(promotion);
+        if (promotionHandler == null) {
+            throw new RuntimeException("没有找到对应的策略, 请检查优惠类型");
+        }
         Integer price = promotionHandler.getPrice(orderVO);
+        //在程序中价格单位为分，所以展示给用户需要转换为元
         BigDecimal bigDecimalPrice = new BigDecimal(price).divide(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_HALF_UP);
-        System.out.println("price = " + bigDecimalPrice);
+        for (GoodsVO goodsVO : orderVO.getGoodsList()) {
+            System.out.println(goodsVO.getName() + ":" + goodsVO.getCount() + "份");
+        }
+        System.out.println("总价为：" + bigDecimalPrice + "元");
     }
 
 }
